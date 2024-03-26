3c7cfbb9fe71823457881c1440c87c0dc77cc415
Alvin Angulu <angulualvin@gmail.com>
2024-03-26 14:49:52 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate"){
+   
+    const msg = `This Saturday, I want to ${activity}!`
+        return msg;
+}
+
+function mondayWork(activity = "go to the office"){
+    const msg = `This Monday, I will ${activity}.`
+    return msg;
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
